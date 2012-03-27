b8d8a7f9b4bdef513e811ef0b373d244ecde4ae0
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-03-27 09:31:52 +0200

    Ugrade of deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+git pull
 git checkout master
 ./build.sh
 git checkout gh-pages
 cp -r -v target/* .
-git add .
-git commit -m'Deploying pages'
-#git push origin gh-pages
-
+git add -A .
+git commit -am 'Deploying pages'
+git checkout master
 
