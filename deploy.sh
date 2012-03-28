f91edaaddc2eaf6c95353bc7b9e60c74a7430e9d
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-03-29 00:02:25 +0200

    Changing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 git pull
+sleep 1
 git checkout master
+sleep 1
 ./build.sh
+sleep 1
 git checkout gh-pages
+sleep 1
 cp -r -v target/* .
+sleep 1
 git add -A .
+sleep 1
 git commit -am 'Deploying pages'
+sleep 1
 git checkout master
-
+sleep 1
