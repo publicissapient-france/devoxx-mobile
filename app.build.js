06b5e37f00eac7a4cb436ea65860d2bdf47a4e33
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-03-29 19:54:41 +0200

    Update of deploy script

--- app.build.js
+++ app.build.js
@@ -17,7 +17,7 @@
     //The directory path to save the output. If not specified, then
     //the path will default to be a directory called "build" as a sibling
     //to the build file. All relative paths are relative to the build file.
-    dir: "./target",
+    dir: "./target/website",
 
     //Set paths for modules. If relative paths, set relative to baseUrl above.
     //If a special value of "empty:" is used for the path value, then that
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-git pull
-git checkout master
+rm -rf target
+mkdir target
+mkdir target/website
 ./build.sh
+mkdir target/repo-clone
+git clone git@github.com:xebia-france/devoxx-mobile.git target/repo-clone
+cd target/repo-clone
 git checkout gh-pages
-cp -r -v target/* .
+cp -r -v ../website/* .
 git add -A .
 git commit -am 'Deploying pages'
 git checkout master
